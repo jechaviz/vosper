614e63b2785d783f12f42f6dec1cdb10ebea7891
jesus <461913@petco.com>
2024-10-11 12:34:37 -0600

    fix: Replace 'clear' with 'cls' to clear the console on Windows

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import vosper, os; vosper = vosper.new()
 while 'listening':
     text = vosper.listen()
     if ('-' in text): print(text)
-    elif (text != ''): os.system('clear'); print('- '+ text)
+    elif (text != ''): os.system('cls'); print('- '+ text)
+#
